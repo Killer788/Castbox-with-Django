f138ce6c1fbce9507c3e14ec45635a96084cba35
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-15 16:18:57 +0330

    Modified settings for signing up.

--- member_area/forms.py
+++ member_area/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class SignUpForm(forms.Form):
-    username = forms.CharField(label='Keyword', max_length=25)
+    username = forms.CharField(label='Username', max_length=25)
     password = forms.CharField(label='Password', max_length=25)
     repeat_password = forms.CharField(label='Repeat Password', max_length=25)
