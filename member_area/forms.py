f98ad9a7443e33c8d0280c3043968acd8d974aaa
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-15 14:53:46 +0330

    Created form for signing up.

---
+++ member_area/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(label='Keyword', max_length=25)
+    password = forms.CharField(label='Password', max_length=25)
+    repeat_password = forms.CharField(label='Repeat Password', max_length=25)
--- member_area/views.py
+++ member_area/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+from .member_handler import MemberHandler
+
+
 # Create your views here.
+def sign_up_view(request):
+    message = ''
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['password'] == form.cleaned_data['repeat_password']:
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                member_handler = MemberHandler(username=username, password=password)
+            else:
+                message = 'Password and repeat password fields should be the same.'
+    else:
+        form = SignUpForm()
+
+    return render(request, 'member_area/sign_up_form.html', {'form': form, 'message': message})
