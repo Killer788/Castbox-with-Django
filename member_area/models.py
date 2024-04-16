a1934e52a631101a340a4545c85a4be6ec67a95e
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-16 14:23:44 +0330

    The User model now extends from Django User model.

--- member_area/models.py
+++ member_area/models.py
@@ -1,10 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 from lib.common_base_models import BaseModelWithUpdatedAt, BaseModelWithIsActive, BaseModelWithTitleAndDescription
 
 
 # Create your models here.
-class User(BaseModelWithIsActive):
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, username, password, **other_fields):
+        user = self.model(username=username, password=password, **other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(username, password, **other_fields)
+
+
+class User(BaseModelWithIsActive, AbstractBaseUser, PermissionsMixin):
     GENDER_CHOICES = (
         ('Hide', 'Hide'),
         ('Male', 'Male'),
@@ -48,6 +71,12 @@ class User(BaseModelWithIsActive):
         verbose_name='Age'
     )
     image_source = models.TextField(null=True, blank=True, verbose_name="Image Source")
+    is_staff = models.BooleanField(default=False, verbose_name="Is Staff")
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'password']
 
     class Meta:
         verbose_name = 'User'
