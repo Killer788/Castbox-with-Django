cb3242acc22ce72896ea61286f7a940fd05adc34
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-16 16:25:34 +0330

    Added email field to User model.

--- member_area/models.py
+++ member_area/models.py
@@ -6,14 +6,18 @@ from lib.common_base_models import BaseModelWithUpdatedAt, BaseModelWithIsActive
 
 # Create your models here.
 class CustomAccountManager(BaseUserManager):
-    def create_user(self, username, password, **other_fields):
-        user = self.model(username=username, password=password, **other_fields)
+    def create_user(self, username, password, email, **other_fields):
+        if not email:
+            raise ValueError('You must provide an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, password=password, email=email, **other_fields)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, username, password, **other_fields):
+    def create_superuser(self, username, password, email, **other_fields):
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
@@ -24,7 +28,7 @@ class CustomAccountManager(BaseUserManager):
         if other_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must be assigned to is_superuser=True.')
 
-        return self.create_user(username, password, **other_fields)
+        return self.create_user(username, password, email, **other_fields)
 
 
 class User(BaseModelWithIsActive, AbstractBaseUser, PermissionsMixin):
@@ -52,6 +56,7 @@ class User(BaseModelWithIsActive, AbstractBaseUser, PermissionsMixin):
         blank=False,
         verbose_name='Username'
     )
+    email = models.EmailField(unique=True, verbose_name='Email')
     gender = models.CharField(
         max_length=10,
         choices=GENDER_CHOICES,
@@ -70,6 +75,7 @@ class User(BaseModelWithIsActive, AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     class Meta:
         verbose_name = 'User'
