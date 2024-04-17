ddfc8cb1cb60c901d690c1ef5f06f81dcf878803
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-17 12:32:31 +0330

    Created endpoint for showing channels.

--- member_area/urls.py
+++ member_area/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from .views import sign_up_view, sign_in_view, sign_out_view, edit_profile_view
+from .views import ChannelViewSet
+
+
+channel_router = routers.DefaultRouter()
+channel_router.register('', ChannelViewSet)
 
 
 urlpatterns = [
     path('signup/', sign_up_view, name='signup'),
     path('signin/', sign_in_view, name='signin'),
     path('signout/', sign_out_view, name='signout'),
-    path('editprofile/', edit_profile_view, name='editprofile')
+    path('editprofile/', edit_profile_view, name='editprofile'),
+    path('channels_list/', include(channel_router.urls)),
 ]
