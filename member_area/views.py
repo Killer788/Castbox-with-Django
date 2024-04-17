921503f7a7ec49b9d4b51a2ddd394f93fe64d453
Parham Sabetkar <parham.sabetkar@gmail.com>
2024-04-17 12:02:58 +0330

    Modified redirect for signin method.

--- member_area/views.py
+++ member_area/views.py
@@ -44,7 +44,7 @@ def sign_in_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                redirect('../')
+                return redirect('../')
             else:
                 messages.info(request, 'Username or Password is incorrect.')
 
